7e41c5cd454844198ad8557811d7d4b4b2757c13
richard lum <a00470425@gmail.com>
2018-07-21 17:26:57 -0700

    seems to be working, pls tst

--- dist/tree.js
+++ dist/tree.js
@@ -3,6 +3,8 @@ const path = require('path');
 let rootdir = '.';
 if (process.argv.length > 2)
     rootdir = process.argv[2];
+//rootdir = path.resolve(rootdir)
+let allfiles = [];
 const isDir = function isDir(fileobj) {
     return new Promise(function (resolve, reject) {
         fs.stat(fileobj.fullpath, function (err, stats) {
@@ -20,11 +22,14 @@ const toFileObj = function toFileObj(name) {
             isdir: false,
             fullpath: path.resolve(name),
             files: [],
-            dirs: [],
+            //dirs:[],
             fileobjs: []
         };
         isDir(fileobj)
-            .then((fileobj) => { return resolve(fileobj); })
+            .then((fileobj) => {
+            //console.log(43,'toFileObj',fileobj);
+            return resolve(fileobj);
+        })
             .catch((err) => reject(err));
     });
 };
@@ -33,8 +38,8 @@ const readobjdir = function readobjdir(fileobj) {
         fs.readdir(fileobj.fullpath, function (err, files) {
             if (err)
                 return reject(err);
-            fileobj.files = files.map((file) => path.resolve(file));
-            //console.log(53, fileobj)
+            fileobj.files = files.map((file) => path.resolve(fileobj.fullpath, file));
+            console.log(53, 'readobjdir', fileobj);
             return resolve(fileobj);
         });
     });
@@ -44,6 +49,7 @@ const readdir = function readdir(dir) {
         fs.readdir(dir, function (err, files) {
             if (err)
                 return reject(err);
+            console.log(66, 'readdir', files);
             return resolve(files.map((file) => path.resolve(file)));
         });
     });
@@ -56,22 +62,31 @@ const classify = function classify(fileobj) {
         });
         Promise.all(fileobjlist)
             .then((fobjlist) => {
-            fileobj.files = fobjlist.filter((fl) => { return !fl.isdir; }).map((fl) => fl.name);
+            fileobj.files = fobjlist.filter((fl) => { return !fl.isdir; }).map((fl) => fl.fullpath);
+            allfiles = allfiles.concat(fileobj.files);
             fileobj.fileobjs = fobjlist.filter((fl) => { return fl.isdir; });
+            console.log(82, 'classify files', fileobj.files);
+            console.log(83, 'classify fileobjs', fileobj.fileobjs);
             return fileobj;
         })
-            .then((fileobj) => { return resolve(fileobj); })
+            .then((fileobj) => { console.log(85, 'classify', fileobj); return (fileobj); })
+            .then(recurse)
+            .then(fobj => { return resolve(fobj); })
             .catch((err) => { return reject(err); });
     });
 };
 const recurse = function recurse(fileobj) {
     return new Promise(function (resolve, reject) {
+        console.log(99, 'recurse', fileobj);
         if (!fileobj.isdir)
             return resolve(fileobj);
+        if (fileobj.fileobjs.length <= 0)
+            return resolve(fileobj);
         fileobj.fileobjs.forEach((fobj) => {
-            console.log(93, fobj);
+            console.log(93, 'recurse entry', fobj);
             readobjdir(fobj)
                 .then(classify)
+                .then((fileobj_ => { console.log(98, 'recurse after classify', fileobj); return resolve(fileobj); }))
                 .catch((err) => { return reject(err); });
         });
     });
@@ -84,7 +99,8 @@ toFileObj(rootdir)
     //.then ( (x) => { console.log(x); return x; })
     .then(readobjdir)
     .then(classify)
-    .then(recurse)
-    .then((x) => { console.log(x); return x; })
+    //.then(recurse)
+    .then((x) => { console.log(114, JSON.stringify(x)); return x; })
+    .then((x) => { console.log(117, allfiles); console.log('total = ', allfiles.length); return x; })
     .catch(console.error);
 //# sourceMappingURL=tree.js.map
--- src/tree.ts
+++ src/tree.ts
@@ -7,13 +7,14 @@ let rootdir = '.'
 if (process.argv.length>2)
   rootdir = process.argv[2];
 //rootdir = path.resolve(rootdir)
+let allfiles:string[] = [];
 
 interface FileObj{
   name:string,
   fullpath:string,
   isdir:boolean,
   files:string[],
-  dirs:string[],
+  //dirs:string[],
   fileobjs:FileObj[]
 }
 
@@ -35,22 +36,25 @@ const toFileObj = function toFileObj(name:string):Promise<FileObj>{
       isdir: false,
       fullpath: path.resolve(name),
       files:[],
-      dirs:[],
+      //dirs:[],
       fileobjs:[]
     }
+    
     isDir(fileobj)
-    .then( (fileobj) => {return resolve (fileobj)})
+    .then( (fileobj) => {
+      //console.log(43,'toFileObj',fileobj);
+      return resolve (fileobj)})
     .catch( (err) => reject(err))    
   })
 }
 
 
-const readobjdir = function readobjdir(fileobj:FileObj):Promise<FileObj>{
+const readobjdir = function readobjdir(fileobj:FileObj):Promise<FileObj>{  
   return new Promise(function(resolve, reject){
-    fs.readdir(fileobj.fullpath,function(err,files){
+    fs.readdir(fileobj.fullpath,function(err,files){  
       if (err) return reject(err)
-      fileobj.files = files.map( (file) => path.resolve(file))
-      //console.log(53, fileobj)
+      fileobj.files = files.map( (file) => path.resolve(fileobj.fullpath,file))
+      console.log(53, 'readobjdir', fileobj)
       return resolve(fileobj)
     })
   })
@@ -62,7 +66,7 @@ const readdir = function readdir(dir:string):Promise<string[]>{
   return new Promise(function(resolve, reject){
     fs.readdir(dir,function(err,files){
       if (err) return reject(err)
-
+      console.log(66,'readdir',files);
       return resolve(files.map( (file) => path.resolve(file)))
     })
   })
@@ -76,25 +80,33 @@ const classify = function classify(fileobj:FileObj):Promise<FileObj>{
     })
     Promise.all(fileobjlist)
     .then( (fobjlist) => {
-      fileobj.files = fobjlist.filter((fl) => { return !fl.isdir }).map( (fl) => fl.name )
+      fileobj.files = fobjlist.filter((fl) => { return !fl.isdir }).map( (fl) => fl.fullpath )
+      allfiles = allfiles.concat(fileobj.files)
       fileobj.fileobjs = fobjlist.filter((fl) => { return fl.isdir })
+      console.log(82,'classify files',fileobj.files)
+      console.log(83,'classify fileobjs',fileobj.fileobjs)
       return fileobj
     })
-    .then( (fileobj) => { return resolve(fileobj) })
+    .then( (fileobj) => { console.log(85,'classify',fileobj); return (fileobj) })
+    .then (recurse)
+    .then ( fobj => { return resolve (fobj)})
     .catch( (err) => { return reject(err) })
   })
 }
 
 
 const recurse = function recurse(fileobj:FileObj):Promise<FileObj>{
-  return new Promise(function(resolve,reject){
+  return new Promise(function(resolve,reject){ console.log(99,'recurse',fileobj)
     if (!fileobj.isdir) return resolve(fileobj)
+    if (fileobj.fileobjs.length <=0) return resolve(fileobj)
     fileobj.fileobjs.forEach( (fobj) => {
-      console.log(93,fobj)
+      console.log(93,'recurse entry',fobj)
       readobjdir(fobj)
       .then(classify)
+      .then( (fileobj_ => { console.log(98,'recurse after classify', fileobj); return resolve(fileobj); } ) )
       .catch( (err) => { return reject(err) })
     })
+
   })
 }
 // readdir(rootdir)
@@ -106,6 +118,7 @@ toFileObj(rootdir)
 //.then ( (x) => { console.log(x); return x; })
 .then(readobjdir)
 .then(classify)
-.then(recurse)
-.then ( (x) => { console.log(x); return x; })
+//.then(recurse)
+.then ( (x) => { console.log(114,JSON.stringify(x)); return x; })
+.then( (x) => {  console.log(117,allfiles);console.log('total = ', allfiles.length); return x; })
 .catch(console.error)
